aa7e8929c5926eb25cf9fe770902befdc888df6e
shailiran <shailiran@gmail.com>
2021-08-06 22:32:37 +0300

    fix header and disclosure

--- src/app.js
+++ src/app.js
@@ -1,20 +1,22 @@
 const RECOMMENDATIONS = 'https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init'
 
 async function getRecommendations() {
-  let response = await fetch(RECOMMENDATIONS);
-  let data = await response.json();
-  return data;
+  try {
+    let response = await fetch(RECOMMENDATIONS);
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error)
+  }
 }
-getRecommendations().then(data => console.log(data.list))
+
+getRecommendations().then(data => console.log(data))
 
 function addElement() {
   getRecommendations().then(data => {
 
-    var header = document.createElement('h3');
-    var headerText = document.createTextNode("You May Like");
-    header.className = 'header';
-    header.appendChild(headerText);
-    document.getElementById('main').appendChild(header);
+    // Add header and disclosure
+    addTop();
 
     var allRecommendations = document.createElement('div');
     allRecommendations.className = 'recommendations'
@@ -26,14 +28,21 @@ function addElement() {
       // Create new elements
       var cardDiv = document.createElement('div');
       cardDiv.className = 'card';
+
+      // Redirect to url
+      cardDiv.addEventListener('click', () => {
+        window.location.replace(element.url)
+      });
+
       var br = document.createElement('br');
       var br1 = document.createElement('br');
+
+      
+      // Add content
       var spanName = document.createElement('span');
       spanName.className = 'name';
       var spanBrandding = document.createElement('span');
       spanBrandding.className = 'brand'
-
-      // Add content
       var name = document.createTextNode(element.name);
       var branding = document.createTextNode(element.branding);
       var img = document.createElement('IMG');
@@ -54,6 +63,26 @@ function addElement() {
     });
     
   })
+}
+
+function addTop() {
+  var top = document.createElement('div');
+  top.className = 'top'
+
+  var header = document.createElement('span');
+  header.className = 'header';
+  var headerText = document.createTextNode("You May Like");
+  header.appendChild(headerText);
+
+  var disclosure = document.createElement('span');
+  disclosure.className = 'disclosure'
+  var disclosureText = document.createTextNode("Sponsored Links by Taboola");
+  disclosure.appendChild(disclosureText);
+
+  top.appendChild(header);
+  top.appendChild(disclosure);
+
+  document.getElementById('main').appendChild(top);
 }
 
 document.body.onload = addElement;
