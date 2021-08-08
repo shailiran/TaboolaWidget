911b1f1538acbff31636f83e244f263f542d6c9d
shailiran <shailiran@gmail.com>
2021-08-08 08:49:54 +0300

    change api name

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-const RECOMMENDATIONS = 'https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init'
+const RECOMMENDATIONS_API = 'https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init'
 
 async function getRecommendations() {
   try {
-    let response = await fetch(RECOMMENDATIONS);
+    let response = await fetch(RECOMMENDATIONS_API);
     let data = await response.json();
-    response = await fetch(RECOMMENDATIONS);
-    data = await response.json();
     return data;
   } catch (error) {
     console.log(error)
@@ -102,7 +100,6 @@ function addAllRecommendations() {
       // Did not get 6 recommendations
       addAllRecommendations();
     } else {
-
       // Add header and disclosure to 'main' div
       createTop();
       // Add recommendtions to 'main' div
